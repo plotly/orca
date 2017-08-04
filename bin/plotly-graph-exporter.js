dffe8f4382ca6385f37b2208b147e4112a46db4d
Etienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2017-08-04 15:47:45 -0400

    misc plotly-graph-exporter cli improvments
    - add --verbose, --parallel-limit and
      component opts (format, scale, width, height)
    - throw error on 'done' when non-200 is received

--- bin/plotly-graph-exporter.js
+++ bin/plotly-graph-exporter.js
@@ -1,4 +1,5 @@
 const plotlyExporter = require('../')
+const cst = require('../src/component/plotly-graph/constants')
 const pkg = require('../package.json')
 const getStdin = require('get-stdin')
 const subarg = require('subarg')
@@ -6,29 +7,42 @@ const fs = require('fs')
 const path = require('path')
 
 const ARGS_CONFIG = {
-  'boolean': ['debug', 'help', 'version'],
-  'string': ['plotly', 'mapbox-access-token', 'mathjax', 'topojson'],
+  'boolean': ['debug', 'help', 'version', 'verbose'],
+  'string': [
+    'plotly', 'mapbox-access-token', 'mathjax', 'topojson',
+    'format', 'scale', 'width', 'height',
+    'parallel-limit'
+  ],
 
   'alias': {
     'help': ['h'],
     'version': ['v'],
     'plotly': ['plotlyjs', 'plotly-js', 'plotly_js', 'plotlyJS', 'plotlyJs'],
     'mapbox-access-token': ['mapboxAccessToken'],
-    'mathjax': ['MathJax']
+    'mathjax': ['MathJax'],
+    'format': ['f'],
+    'parallel-limit': ['parallelLimit']
   },
 
   'default': {
     'debug': false,
     'help': false,
     'version': false,
+    'verbose': false,
     'plotly': 'latest',
     'mapbox-access-token': process.env.mapboxAccessToken || '',
     'mathjax': '',
-    'topojson': ''
+    'topojson': '',
+    'format': '',
+    'scale': '',
+    'width': '',
+    'height': '',
+    'parallel-limit': ''
   }
 }
 
 const argv = subarg(process.argv.slice(2), ARGS_CONFIG)
+const showLogs = argv.debug || argv.verbose
 
 if (argv.version) {
   console.log(pkg.version)
@@ -46,7 +60,7 @@ if (argv.help) {
 
   Usage:
 
-    $ plotly-graph-exporter [path/to/json/file(s) URL(s), glob(s), '{"data":[],"layout":{}}'] {options}
+    $ plotly-graph-exporter [path/to/json/file(s), URL(s), glob(s), '{"data":[],"layout":{}}'] {options}
 
     $ cat plot.json | plotly-graph-exporter {options}
 
@@ -58,9 +72,9 @@ if (argv.help) {
   --version ${showAliases('version')}
     Displays package version.
 
-  --plotly ${showAliases('plotly.js')}
-    Sets the path to the plotly.js to use.
-    This option can be also set to 'latest' or any valid plotly.js server release (e.g. '1.2.3'),
+  --plotly ${showAliases('plotly')}
+    Sets the path to the plotly.js bundle to use.
+    This option can be also set to 'latest' or any valid plotly.js server release (e.g. 'v1.2.3'),
     where the corresponding plot.ly CDN bundle is used.
     By default, the 'latest' CDN bundle is used.
 
@@ -74,20 +88,32 @@ if (argv.help) {
   --mathjax ${showAliases('mathjax')}
     Sets path to MathJax files. Required to export LaTeX characters.
 
-  --format
+  --format ${showAliases('format')}
+    Sets the output format (${Object.keys(cst.contentFormat).join(', ')}). Applies to all output images.
 
   --scale
+    Sets the image scale. Applies to all output images.
 
   --width
+    Sets the image width. If not set, defaults to \`layout.width\` value. Applies to all output images.
 
   --height
+    Sets the image height. If not set, defaults to \`layout.height\` value. Applies to all output images.
+
+  --parallel-limit ${showAliases('parallel-limit')}
+    Sets the limit of parallel tasks run.
+
+  --verbose
+    Turn on verbose logging on stdout.
 
   --output (??)
+
+  --output-dir (??)
 
   --overwrite (??)
 
   --debug
-    Starts app in debug mode.
+    Starts app in debug mode and turn on verbose logs on stdout.
   `)
   process.exit(0)
 }
@@ -102,20 +128,28 @@ getStdin().then((txt) => {
   const app = plotlyExporter.run({
     input: hasStdin ? argv._.concat([txt]) : argv._,
     debug: argv.debug,
+    parallelLimit: argv.parallelLimit,
     component: {
       name: 'plotly-graph',
       options: {
-        plotlyJS: argv.plotly,
+        plotlyJS: path.resolve(argv.plotly),
         mapboxAccessToken: argv['mapbox-access-token'],
         mathjax: argv.mathjax,
-        topojson: argv.topojson
+        topojson: argv.topojson,
+        format: argv.format,
+        scale: argv.scale,
+        width: argv.width,
+        height: argv.height
       }
     }
   })
 
   app.on('after-export', (info) => {
     const itemName = getItemName(info)
-    console.log(`exported ${itemName}, in ${info.processingTime} ms`)
+
+    if (showLogs) {
+      console.log(`exported ${itemName}, in ${info.processingTime} ms`)
+    }
 
     fs.writeFile(`${itemName}.${info.format}`, info.body, (err) => {
       if (err) throw err
@@ -124,21 +158,37 @@ getStdin().then((txt) => {
 
   app.on('export-error', (info) => {
     const itemName = getItemName(info)
-    console.log(`export error ${info.code} for ${itemName} - ${info.msg}`)
+
+    if (showLogs) {
+      console.warn(`export error ${info.code} for ${itemName} - ${info.msg}`)
+      console.warn(`  ${info.error}`)
+    }
   })
 
-  // TODO more descriptive event name?
   app.on('done', (info) => {
-    console.log(`\ndone with code ${info.code} - ${info.msg}`)
+    const msg = `done with code ${info.code} in ${info.totalProcessingTime} ms - ${info.msg}`
+
+    if (info.code === 200) {
+      console.log('\n' + msg)
+    } else if (argv.debug) {
+      console.error('\n' + msg)
+      console.lef('  leaving window open for debugging')
+    } else {
+      throw new Error(msg)
+    }
   })
 
   app.on('renderer-error', (info) => {
-    console.warn(`${info.msg} - ${info.error}`)
-    app.quit()
+    if (showLogs) {
+      console.warn(`${info.msg} - ${info.error}`)
+    }
   })
 
   process.on('uncaughtException', (err) => {
     console.warn(err)
-    app.quit()
+
+    if (!argv.debug) {
+      app.quit()
+    }
   })
 })
