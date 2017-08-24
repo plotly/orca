b2b3ea38a2dacbf071451ad33e19e61539fbd102
Etienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2017-08-24 12:39:40 -0400

    add pdftops util (to convert pdf2eps)

---
+++ test/unit/pdftops_test.js
@@ -0,0 +1,82 @@
+const tap = require('tap')
+const sinon = require('sinon')
+const path = require('path')
+const fs = require('fs')
+const childProcess = require('child_process')
+const Pdftops = require('../../src/util/pdftops')
+
+const { paths } = require('../common')
+const PDF_MOCK = fs.readFileSync(path.join(paths.build, '20.pdf'))
+
+tap.test('pdftops.pdf2eps', t => {
+  t.test('should convert pdf to eps', t => {
+    const pdftops = new Pdftops()
+    const outPath = path.join(paths.build, 'pdftops-test.eps')
+
+    pdftops.pdf2eps(PDF_MOCK, {}, (err, result) => {
+      if (err) t.fail(err)
+      t.type(result, Buffer)
+
+      fs.writeFile(outPath, result, (err) => {
+        if (err) t.fail(err)
+
+        const size = fs.statSync(outPath).size
+        t.ok(size > 1e6, 'min pdf file size')
+        t.ok(size < 2e6, 'max pdf file size')
+        t.end()
+      })
+    })
+  })
+
+  t.test('should remove tmp files after conversion', t => {
+    const pdftops = new Pdftops()
+    const tmpOutPath = path.join(paths.build, 'tmp-eps')
+    const tmpSvgPath = path.join(paths.build, 'tmp-pdf')
+
+    pdftops.pdf2eps(PDF_MOCK, {id: 'tmp'}, (err, result) => {
+      if (err) t.fail(err)
+
+      t.type(result, Buffer)
+      t.notOk(fs.existsSync(tmpOutPath), 'clears tmp eps file')
+      t.notOk(fs.existsSync(tmpSvgPath), 'clears tmp pdf file')
+      t.end()
+    })
+  })
+
+  t.test('should error out when pdftops command fails', t => {
+    const pdftops = new Pdftops('not gonna work')
+
+    const tmpOutPath = path.join(paths.build, 'tmp-eps')
+    const tmpSvgPath = path.join(paths.build, 'tmp-pdf')
+
+    pdftops.pdf2eps(PDF_MOCK, {id: 'tmp'}, (err) => {
+      t.throws(() => { throw err }, /Command failed/)
+      t.notOk(fs.existsSync(tmpOutPath), 'clears tmp eps file')
+      t.notOk(fs.existsSync(tmpSvgPath), 'clears tmp pdf file')
+      t.end()
+    })
+  })
+
+  t.end()
+})
+
+tap.test('isPdftopsInstalled', t => {
+  t.afterEach((done) => {
+    childProcess.execSync.restore()
+    done()
+  })
+
+  t.test('should return true when binary execute correctly', t => {
+    sinon.stub(childProcess, 'execSync').returns(true)
+    t.ok(Pdftops.isPdftopsInstalled())
+    t.end()
+  })
+
+  t.test('should return false when binary does not execute correctly', t => {
+    sinon.stub(childProcess, 'execSync').throws()
+    t.notOk(Pdftops.isPdftopsInstalled())
+    t.end()
+  })
+
+  t.end()
+})
