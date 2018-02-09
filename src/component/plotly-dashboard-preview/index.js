fb96e99fce734ad9032a50e1cd165f200b05bcec
Jody McIntyre <scjody@modernduck.com>
2018-02-09 15:36:14 -0500

    Add "ping" function to all components

--- src/component/plotly-dashboard-preview/index.js
+++ src/component/plotly-dashboard-preview/index.js
@@ -2,6 +2,7 @@ const plotlyGraph = require('../plotly-graph')
 
 module.exports = {
   name: 'plotly-dashboard-preview',
+  ping: require('../../util/generic-ping'),
   inject: plotlyGraph.inject,
   parse: require('./parse'),
   render: require('./render'),
