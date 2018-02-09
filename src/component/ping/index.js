7fa6999efbf4ee0fe87a1baa3b0ba0f18c0cf840
Jody McIntyre <scjody@modernduck.com>
2018-02-09 10:58:23 -0500

    Revert "TEMP: use plotly-graph renderer"
    This reverts commit 5ba24f7df429a1c0a8b187dbc0a6bab7112c1098.

--- src/component/ping/index.js
+++ src/component/ping/index.js
@@ -1,11 +1,5 @@
-const plotlyGraph = require('../plotly-graph')
-
 module.exports = {
   name: 'ping',
-  inject: plotlyGraph.inject,
-  parse: plotlyGraph.parse,
-  //render: plotlyGraph.render,
   render: require('./render'),
-  convert: plotlyGraph.convert
-  //convert: require('./convert')
+  convert: require('./convert')
 }
