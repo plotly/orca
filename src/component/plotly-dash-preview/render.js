5de656ea13bbfcc178a3b6ff4462b9c2b8960026
tarzzz <tarun.gaba7@gmail.com>
2018-03-23 16:53:39 +0530

    Added better check for finished_loading (rendering)

--- src/component/plotly-dash-preview/render.js
+++ src/component/plotly-dash-preview/render.js
@@ -13,6 +13,7 @@ function render (info, opts, sendToMain) {
   const result = {}
 
   let win = remote.createBrowserWindow()
+  win.loadURL(info.url)
 
   const contents = win.webContents
   let errorCode = null
@@ -26,23 +27,42 @@ function render (info, opts, sendToMain) {
     sendToMain(errorCode, result)
   }
 
+  const finishedLoading = () => {
+    return win.webContents.executeJavaScript(`
+      new Promise((resolve, reject) => {
+        var a = document.getElementById("_finished_loading");
+        if (a) {
+          resolve(a.offsetTop);
+        }
+      })`).then(height => {
+        if (height > 50) {
+          return true;
+        } else {
+          return false
+        }
+      })
+  }
+
   win.on('closed', () => {
     win = null
   })
 
-  contents.on('page-favicon-updated', () => {
-      contents.printToPDF({}, (err, pdfData) => {
-        if (err) {
-            done(525)
+  let intervalId = setInterval(() => {
+    finishedLoading().then(loaded => {
+        if (loaded) {
+          clearInterval(intervalId);
+          contents.printToPDF({}, (err, pdfData) => {
+            if (err) {
+                done(525)
+            }
+            else {
+              result.imgData = pdfData
+              done()
+            }
+          })        
         }
-        else {
-          result.imgData = pdfData
-          done()
-        }
-      })
-  })
+    })}, 500)
 
-  win.loadURL(info.url)
 }
 
 module.exports = render
