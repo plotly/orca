27275ca47cf11ddae515fd942c2027a9fc9a17c9
tarzzz <tarun.gaba7@gmail.com>
2018-03-27 22:50:19 +0530

    Added more tests.

--- test/unit/plotly-dash-preview_test.js
+++ test/unit/plotly-dash-preview_test.js
@@ -15,12 +15,35 @@ tap.test('parse:', t => {
       t.test(`(case ${JSON.stringify(d)})`, t => {
         fn({url: d}, {}, (errorCode, result) => {
           t.equal(errorCode, 400)
+          t.same(result, {"msg": "invalid url"})
           t.end()
         })
       })
     })
 
     t.end()
+  })
+
+  t.test('should error when neither loading_selector or timeout is given', t => {
+    
+    fn({url: 'https://dash-app.com'}, {}, (errorCode, result) => {
+      t.equal(errorCode, 400)
+      t.equal(result.msg, 'either loading_selector or timeout must be specified')
+      t.end()
+    })
+  })
+
+  t.test('should error when pageSize is not given', t => {
+    
+    fn({
+      url: 'https://dash-app.com',
+      loading_selector: 'dummy',
+    }, {}, (errorCode, result) => {
+      t.equal(errorCode, 400)
+      t.same(result.msg, 'pageSize must either be A3, A4, A5, Legal, Letter, ' + 
+                         'Tabloid or an Object containing height and width in microns.')
+      t.end()
+    })
   })
 
   t.end()
@@ -47,6 +70,23 @@ tap.test('render:', t => {
       t.ok(win.close.calledOnce)
       t.equal(errorCode, undefined, 'code')
       t.equal(result.imgData, '-> image data <-', 'result')
+      t.end()
+    })
+  })
+
+  t.test('should handle executeJavascript errors', t => {
+    const win = createMockWindow()
+    sinon.stub(remote, 'createBrowserWindow').returns(win)
+    win.webContents.executeJavaScript.rejects('fail to load')
+    win.webContents.printToPDF.yields(null, '-> image data <-')
+
+    fn({
+      url: 'https://dummy.com'
+    }, {}, (errorCode, result) => {
+      t.ok(win.webContents.printToPDF.notCalled)
+      t.ok(win.close.calledOnce)
+      t.equal(errorCode, 525, 'code')
+      t.same(result, {"msg": "dash preview generation failed"}, 'result')
       t.end()
     })
   })
