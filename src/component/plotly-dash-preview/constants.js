4f50129d8512c56b650dfee95a23f00036757a85
tarzzz <tarun.gaba7@gmail.com>
2018-03-27 18:26:49 +0530

    Added pageSize/browserSize..
    Size can be specified by:
    * pdf_options: pageSize key (“Letter”, “A3”, “A4” etc.)
    * pageSize in request data

--- src/component/plotly-dash-preview/constants.js
+++ src/component/plotly-dash-preview/constants.js
@@ -1,4 +1,16 @@
+const pixelsInInch = 96
+const micronsInInch = 25400
+
 module.exports = {
   minInterval: 500,
-  maxRenderingTries: 100
+  maxRenderingTries: 100,
+  pixelsInMicron: pixelsInInch / micronsInInch,
+  sizeMapping: {
+    'A3': {'width': 11.7 * pixelsInInch, 'height': 16.5 * pixelsInInch},
+    'A4': {'width': 8.3 * pixelsInInch, 'height': 11.7 * pixelsInInch},
+    'A5': {'width': 5.8 * pixelsInInch, 'height': 8.3 * pixelsInInch},
+    'Letter': {'width': 8.5 * pixelsInInch, 'height': 11 * pixelsInInch},
+    'Legal': {'width': 8.5 * pixelsInInch, 'height': 14 * pixelsInInch},
+    'Tabloid': {'width': 11 * pixelsInInch, 'height': 17 * pixelsInInch}
+  }
 }
