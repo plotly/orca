4a25b60d8427b2d1a775a4a483d9e17c250b5cbb
tarzzz <tarun.gaba7@gmail.com>
2018-04-05 11:41:42 +0530

    Bypass certs error for dash-apps preview.

--- src/app/server/create-server.js
+++ src/app/server/create-server.js
@@ -151,6 +151,11 @@ function createServer (app, BrowserWindow, ipcMain, opts) {
     })
   })
 
+  app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+      event.preventDefault()
+      callback(true)
+  })
+
   server.on('error', (err) => {
     console.error(err)
 
