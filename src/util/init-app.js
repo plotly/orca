813daec414cb5f951f16cef15aa0e135ada92569
etienne <etienne@plot.ly>
2018-04-18 10:28:20 -0400

    handle 'event-gpu-process-crashed'
    - see https://electronjs.org/docs/all#event-gpu-process-crashed

--- src/util/init-app.js
+++ src/util/init-app.js
@@ -1,8 +1,7 @@
-const RENDERER_ERROR_MSG = 'renderer error'
-
 /** Small (common) wrapper for electron app that:
- *  - registers renderer-error ipc listener
  *  - adds command line flag to make WebGL work in headless envs
+ *  - registers renderer-error ipc listener
+ *  - handle gpu process crashed events
  *
  * @param {electron app} app
  * @param {ipcMain} ipcMain
@@ -13,7 +12,15 @@ function initApp (app, ipcMain) {
   ipcMain.on('renderer-error', (event, info) => {
     app.emit('renderer-error', {
       code: 501,
-      msg: RENDERER_ERROR_MSG,
+      msg: 'renderer error',
+      error: info
+    })
+  })
+
+  app.on('gpu-process-crashed', (event, info) => {
+    app.emit('renderer-error', {
+      code: 501,
+      msg: 'gpu process crashed',
       error: info
     })
   })
