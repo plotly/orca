114072d8e4f39fa57ffcd9a54cfc7f85609b7379
Étienne Tétreault-Pinard <etienne@plot.ly>
2018-04-18 09:35:28 -0400

    Run integration tests in retry loop (#76)
    * run integration test in 5x retry loop
    
    - as a workaround those annoying intermittent test timeouts

---
+++ .circleci/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# override CircleCi's default run settings,
+# so that we run all tests and do not exit early
+# on test failures.
+set +e
+set +o pipefail
+
+EXIT_STATE=0
+MAX_AUTO_RETRY=5
+
+# inspired by https://unix.stackexchange.com/a/82602
+retry () {
+    local n=0
+
+    until [ $n -ge $MAX_AUTO_RETRY ]; do
+        "$@" && break
+        n=$[$n+1]
+        echo ''
+        echo run $n of $MAX_AUTO_RETRY failed, trying again ...
+        echo ''
+        sleep 15
+    done
+
+    if [ $n -eq $MAX_AUTO_RETRY ]; then
+        EXIT_STATE=1
+    fi
+}
+
+npm run pretest
+npm run test:lint || EXIT_STATE=$?
+npm run test:unit || EXIT_STATE=$?
+retry npm run test:integration
+exit $EXIT_STATE
