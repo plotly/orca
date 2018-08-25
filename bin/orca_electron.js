02ec2769293ced6a3608a7a606ed55baab0013b3
Jon Mease <jon.mease@gmail.com>
2018-08-24 20:09:06 -0400

    jslint fixes

--- bin/orca_electron.js
+++ bin/orca_electron.js
@@ -44,44 +44,42 @@ if (process.platform === 'darwin' && process.argv.length === 1) {
       'To create your first image, open a terminal and run:\n\norca graph \'{ "data": [{"y": [1,2,1]}] }\' -o fig.png'
   }
 
-  let showMessage = false;
+  let showMessage = false
 
   try {
-      execSync('which orca');
-      // Orca is already on the path, nothing to do
-    } catch (err) {
+    execSync('which orca')
+    // Orca is already on the path, nothing to do
+  } catch (err) {
+    // Check if this is a standalone installation (not conda or npm)
+    const standalonePath = '/Applications/orca.app/Contents/MacOS/orca'
+    if (fs.existsSync(standalonePath)) {
+      // Now we know that orca is not on the path, but it is installed
+      // in the /Applications directory.  So we'll ask the user if they
+      // want to add it to the path
+      const source = path.join(__dirname, 'orca.sh')
+      const target = '/usr/local/bin/orca'
 
-      // Check if this is a standalone installation (not conda or npm)
-      const standalonePath = '/Applications/orca.app/Contents/MacOS/orca'
-      if (fs.existsSync(standalonePath)) {
+      if (!fs.existsSync(target)) {
+        // Build copy command
+        const copyCmd = `"cp ${source} ${target}"`
 
-        // Now we know that orca is not on the path, but it is installed
-        // in the /Applications directory.  So we'll ask the user if they
-        // want to add it to the path
-        const source = path.join(__dirname, 'orca.sh')
-        const target = '/usr/local/bin/orca'
+        // Use apple script to perform copy so that we can launch a GUI
+        // prompt for administrator credentials
+        const prompt = '"Add orca to system PATH (/usr/local/bin)?"'
+        const cmd = `osascript -e 'do shell script ${copyCmd} with prompt ${prompt} with administrator privileges'`
 
-        if (!fs.existsSync(target)) {
-          // Build copy command
-          const copyCmd = `"cp ${source} ${target}"`;
-
-          // Use apple script to perform copy so that we can launch a GUI
-          // prompt for administrator credentials
-          const prompt = '"Add orca to system PATH (/usr/local/bin)?"';
-          const cmd = `osascript -e 'do shell script ${copyCmd} with prompt ${prompt} with administrator privileges'`;
-
-          try {
-            execSync(cmd);
-            showMessage = true;
-          } catch (cmdErr) {
-            // User cancelled. Nothing more to do
-          }
+        try {
+          execSync(cmd)
+          showMessage = true
+        } catch (cmdErr) {
+          // User cancelled. Nothing more to do
         }
-      } else {
-        options.message = "Executable in non-standard location"
-        options.detail = "No orca executable located at /Applications/orca.app/\nNo changes made"
-        showMessage = true;
       }
+    } else {
+      options.message = 'Executable in non-standard location'
+      options.detail = 'No orca executable located at /Applications/orca.app/\nNo changes made'
+      showMessage = true
+    }
   }
 
   app.on('ready', function () {
@@ -91,7 +89,6 @@ if (process.platform === 'darwin' && process.argv.length === 1) {
     console.log(HELP)
     process.exit(options.type === 'error' ? 1 : 0)
   })
-
 } else if (opts._.length) {
   const cmd = opts._[0]
   const cmdArgs = args.slice(1)
