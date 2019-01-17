1cedf4ad6145a59d6d1c79a50fa553f2e9254c34
Antoine Roy-Gobeil <antoine@plot.ly>
2019-01-17 09:52:21 -0500

    test that no script tags are referencing HTTP resources

---
+++ test/integration/orca_serve_offline_test.js
@@ -0,0 +1,82 @@
+const tap = require('tap')
+const Application = require('spectron').Application
+const { webContents } = require('electron')
+
+const request = require('request')
+const { paths } = require('../common')
+
+const PORT = 9109 + 2
+const SERVER_URL = `http://localhost:${PORT}`
+
+const numberOfComponents = 6;
+const axios = require('axios');
+const fs = require('fs');
+const pathToPlotlyJS = "/tmp/plotly-latest.min.js"
+
+const app = new Application({
+  path: paths.bin,
+  args: ['serve', '--port', PORT, '--plotlyjs', pathToPlotlyJS]
+})
+
+tap.test('should launch', t => {
+  var plotlyJS = fs.createWriteStream(pathToPlotlyJS);
+  axios.request({
+    url: 'https://cdn.plot.ly/plotly-latest.min.js',
+    method: 'get',
+  }).then((result) => {
+    fs.writeFileSync(pathToPlotlyJS, result.data);
+  })
+  .then(() => {
+    return app.start()
+  })
+  .then(() => {
+    app.client.getWindowCount().then(cnt => {
+      t.equal(cnt, numberOfComponents)
+      t.end()
+    })
+  })
+  .catch(err => {
+    t.fail()
+    t.end()
+  })
+})
+
+function getScriptTagsSrc(index) {
+  // executeJavaScript in Spectron is broken https://github.com/electron/spectron/issues/163
+  return app.client.windowByIndex(index).then(() => {
+    return app.client.execute(() => {
+      var htmlCollection = document.getElementsByTagName('script')
+      var arr = [].slice.call(htmlCollection);
+      return arr.map(script => {
+        return script.src
+      })
+    })
+  })
+}
+
+tap.test('should not link to resources on the network', t => {
+  var promises = []
+  for(var i = 0; i < numberOfComponents; i++) {
+    promises.push(getScriptTagsSrc(0))
+  }
+  Promise.all(promises).then(values => {
+    values.forEach(result => {
+      var urls = result.value
+      urls.forEach(url => {
+        t.notOk(url.match('http'), `A script tag refers to an HTTP resource ${url}`)
+      })
+    })
+    t.end()
+  })
+  .catch(err => {
+    t.fail()
+    t.end()
+  })
+})
+
+
+tap.test('should teardown', t => {
+  app.stop()
+    .catch(t.fail)
+    .then(t.end)
+})
