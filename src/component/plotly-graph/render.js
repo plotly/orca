2007a7d25138e5a2c6bc4495c0a74059556bd166
Antoine Roy-Gobeil <antoine@plot.ly>
2020-04-03 14:25:14 -0400

    fix lint

--- src/component/plotly-graph/render.js
+++ src/component/plotly-graph/render.js
@@ -68,8 +68,7 @@ function render (info, opts, sendToMain) {
     // 'full-json' was introduced in plotly.js v1.53.0
     // see: https://github.com/plotly/plotly.js/releases/tag/v1.53.0
     format === 'full-json' && semver.lte(Plotly.version, '1.53.0')
-  )
-  {
+  ) {
     errorCode = 527
     result.error = `plotly.js version: ${Plotly.version}`
     return done()
